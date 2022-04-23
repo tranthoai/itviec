472517fde1e599024fb0c5d0544ae682654b5e71
Thoai Tran <tranthoaipy@gmail.com>
2022-04-24 04:19:50 +0700

    Startup ITViec

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Frontend</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="frontend/development/favicon.ico">
+    <link rel="stylesheet" href="frontend/development/styles.css">
+</head>
+<body>
+<app-root></app-root>
+<script src="frontend/development/runtime.js" type="module"></script>
+<script src="frontend/development/polyfills.js" type="module"></script>
+<script src="frontend/development/vendor.js" type="module"></script>
+<script src="frontend/development/main.js" type="module"></script>
 
-use App\Kernel;
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+</body>
+</html>

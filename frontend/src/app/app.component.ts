46380ddb0718a47559a43604365735d7e87624cd
Thoai Tran <tranthoaipy@gmail.com>
2022-04-24 20:09:35 +0700

    Startup ITViec

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Product } from './domain/product';
+import { ProductService } from './service/productservice';
+import {PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  products: Product[] = [];
+
   title = 'frontend';
+
+  constructor(
+    private productService: ProductService,
+    private primengConfig: PrimeNGConfig
+  ) {
+
+  }
+
+  ngOnInit() {
+
+    this.primengConfig.ripple = true;
+
+    this.productService.getProducts().then(
+      (data) => {
+
+        this.products = data;
+
+      });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {TableModule} from "primeng/table";
+import {ProductService} from "./service/productservice";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TableModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
